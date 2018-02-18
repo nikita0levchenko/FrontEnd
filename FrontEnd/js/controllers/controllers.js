61420f0b5b52d698074d6d675aed6e9044fd489e
Nikita Levchenko <nikita0levchenko@gmail.com>
2018-02-19 02:09:07 +0300

    Menu + mobile menu

--- FrontEnd/js/controllers/controllers.js
+++
@@ -1,9 +0,0 @@
-app.controller('Homepage',['$scope',function($scope){
-    $scope.homepage= "Главная";
-}]);
-app.controller('Contacts',['$scope',function($scope){
-    $scope.contacts=new Date();
-}]);
-app.controller('Tournaments',['$scope',function ($scope){
-    $scope.tournaments="Турниры";
-}]);
